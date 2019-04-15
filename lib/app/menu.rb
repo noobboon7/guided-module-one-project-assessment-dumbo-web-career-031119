15aaa225e50f5a9488be72c95bdc706e38f7b0fb
Jerry Velazquez <jvr572@gmail.com>
2019-04-15 15:39:45 -0400

    "test"

--- lib/app/menu.rb
+++ lib/app/menu.rb
@@ -2,17 +2,17 @@
 $prompt = TTY::Prompt.new
 $current_user = nil
 #########################
-# clears the user screen
+# presses a key to continue
 def keypress
   $prompt.keypress("Press space or enter to continue", keys: [:space, :return])
 end
-
+# clears the user screen
 def clear
   system "clear"
 end
 def run_program
   welcome
-  sleep(4)
+  sleep(3)
   opening_menu
 end
 def welcome
@@ -31,7 +31,7 @@ _,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
        \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
         \_.-'       |__|    `-._ |              '-.|     '-.| |   |
                                 `'                            '-._|"
-  sleep(4)
+  sleep(3)
   puts "My name is Oak! People call me the pokémon Prof!"
   sleep(1)
   puts "This world is inhabited by creatures called pokémon! For some people, pokémon are pets. Others use them for fights. Myself...I study pokémon as a profession."
@@ -56,21 +56,30 @@ def sign_up
   end
 
   ball = $prompt.decorate("◓",:red)
+
   pass = $prompt.mask("Create a password(4 to 10 characters)",require:true, mask:ball) do |p|
-    p.validate(/[a-z\ ]{4,10}/)
+    binding.pry
+    p.validate(/[a-z,0-9\ ]{4,10}/)
+  end
+
+  gender = $prompt.select('what is your gender?') do |g|
+    binding.pry
+    g.choice 'Male'
+    g.choice 'Female'
+    g.choice 'Non-binary'
   end
 
   if Trainer.find_by(name: name) != nil
     puts "Sorry, username that is taken. Try again."
   else
-    $current_user = Trainer.create(name: name, password: password)
+    $current_user = Trainer.create(name: name, password: pass)
     puts "You have signed up and successfully logged in. Enjoy!"
   end
 end
 
-def log_in
-  $current_user = Trainer.find_by(name: name, password: password)
+def log_in #username, #password
+  $current_user = Trainer.find_by(name: name, password: pass)
   if $current_user == nil
     puts "Your username or password is incorrect. Please try again."
-  end 
+  end
 end
